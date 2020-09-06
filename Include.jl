041eb52f929ee57154d2ed37e75780311356dbc9
varnerlab <jdv27@cornell.edu>
2020-09-06 15:35:56 -0400

    Working on Reimann notebook
    Hard going - but got auto project activation to work

--- Include.jl
+++ Include.jl
@@ -1,12 +1,30 @@
-# declare some path constants -
+# Pre-condition - paths need to be loaded
+
+# do we start in the examples directory?
+# Yes: then cd .. to get into the root
+_tmp_path_dirname = pwd()
+if (occursin("example",_tmp_path_dirname) == true)
+    # go up one dir -
+    cd("..")
+end
+
+# Setup the paths -
 const _PATH_TO_ROOT = pwd()
 const _PATH_TO_SRC = "$(_PATH_TO_ROOT)/src"
 const _PATH_TO_DATA = "$(_PATH_TO_ROOT)/data"
 
+# need to use the package manager, to activate this project -
+import Pkg
+Pkg.activate(_PATH_TO_ROOT);
+
 # include external packages -
 using Pluto
+using DataFrames
+using Gadfly
+using Cairo
+using Fontconfig
 
 # include my codes -
-include("$(_PATH_TO_SRC)/Types.jl")
-include("$(_PATH_TO_SRC)/Factory.jl")
-include("$(_PATH_TO_SRC)/Pressure.jl")
+include("$(_PATH_TO_SRC)/base/Types.jl")
+include("$(_PATH_TO_SRC)/base/Factory.jl")
+include("$(_PATH_TO_SRC)/base/Pressure.jl")
